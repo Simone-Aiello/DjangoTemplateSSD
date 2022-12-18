01609992d14063521ba6107b7f72b33ae31cc74c
simone <simonaiello2000@outlook.com>
2022-12-18 18:12:42 +0100

    test skeleton

---
+++ tests/test_urls.py
@@ -0,0 +1,16 @@
+import json
+import pytest
+from rest_framework.test import APIClient
+
+
+def get_client(user=None):
+    res = APIClient()
+    if user is not None:
+        res.force_login(user)
+    return res
+
+
+def parse(response):
+    response.render()
+    content = response.content.decode()
+    return json.loads(content)
